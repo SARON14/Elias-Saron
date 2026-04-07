5f9ac402c435b88c2cf7651334a10ab212243a39
SARON14 <sharonabi14@gmail.com>
2026-04-07 17:09:01 +0300

    Add wrangler config file fix

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "standalone",
+    experimental: {
+        runtime: "nodejs"
+    }
+};
 
 module.exports = nextConfig;
